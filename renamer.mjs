ecb7dacfb0da67669fdf513ae24765c0aa3bc0ad
alessandro stoppato <alexrah@gmail.com>
2022-04-21 22:40:37 +0200

    renamer.mjs: use fs to get file creation date

--- renamer.mjs
+++ renamer.mjs
@@ -4,7 +4,18 @@ import path from "path";
 class Renamer{
     replace (filePath) {
         const oFile = path.parse(filePath);
-        const sNewName = oFile.name + ' [DONE]' + oFile.ext
+
+        const oStats = fs.statSync(filePath);
+
+        let sMonth = oStats.birthtime.getMonth() + 1;
+        sMonth = sMonth.toString().padStart(2,'0');
+
+        const sDay = oStats.birthtime.getDate().toString().padStart(2,'0');
+
+        const sRand = Math.round(Math.random()*1000000);
+
+        const sNewName = 'IMG_' + oStats.birthtime.getFullYear() + sMonth + sDay + '_' + sRand + oFile.ext
+
         return path.join(oFile.dir, sNewName)
     }
 }
--- test_renamer.mjs
+++ test_renamer.mjs
@@ -1,4 +1,4 @@
 import renamer from "./renamer.mjs";
 
 const ren =  new renamer();
-console.log(ren.replace('/Users/ale/Pictures/IMG_000097_20170514.jpeg'));
+console.log(ren.replace('/Users/ale/Pictures/5/5A3BE0DD-EC89-4616-82A6-05B1485D804F.jpeg'));
